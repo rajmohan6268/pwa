7082e872b391a4d466949d0d86fddee1a9669baf
rajmohan <rajmohan@techaffinity.com>
2021-05-21 13:53:32 +0530

    s

--- sw.js
+++ sw.js
@@ -19,42 +19,42 @@ self.addEventListener('install', function(e) {
 });
 
 /* Serve cached content when offline */
-// self.addEventListener('fetch', function(e) {
-//     e.respondWith(
-//         caches.match(e.request).then(function(response) {
-//             return response || fetch(e.request);
-//         })
-//     )
-//     e.waitUntil(
-//         update(e.request)
-//         .then(refresh)
-//     );
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    )
+    e.waitUntil(
+        update(e.request)
+        .then(refresh)
+    );
 
-// });
+});
 
 
-// function update(request) {
-//     return caches.open(cacheName).then(function(cache) {
-//         return fetch(request).then(function(response) {
-//             return cache.put(request, response.clone()).then(function() {
-//                 return response;
-//             });
-//         });
-//     });
-// }
+function update(request) {
+    return caches.open(cacheName).then(function(cache) {
+        return fetch(request).then(function(response) {
+            return cache.put(request, response.clone()).then(function() {
+                return response;
+            });
+        });
+    });
+}
 
-// function refresh(response) {
-//     return self.clients.matchAll().then(function(clients) {
-//         clients.forEach(function(client) {
+function refresh(response) {
+    return self.clients.matchAll().then(function(clients) {
+        clients.forEach(function(client) {
 
-//             var message = {
-//                 type: 'refresh',
-//                 url: response.url,
+            var message = {
+                type: 'refresh',
+                url: response.url,
 
-//                 eTag: response.headers.get('ETag')
-//             };
+                eTag: response.headers.get('ETag')
+            };
 
-//             client.postMessage(JSON.stringify(message));
-//         });
-//     });
-// }
+            client.postMessage(JSON.stringify(message));
+        });
+    });
+}
