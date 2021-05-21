564be836c18aa7954248e0cb641eabba91938807
rajmohan <rajmohan@techaffinity.com>
2021-05-21 14:09:54 +0530

    s

--- sw.js
+++ sw.js
@@ -21,14 +21,14 @@ self.addEventListener('install', function(e) {
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
     e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
-    )
-    e.waitUntil(
-        update(e.request)
-        .then(refresh)
-    );
+            caches.match(e.request).then(function(response) {
+                return response || fetch(e.request);
+            })
+        )
+        // e.waitUntil(
+        //     update(e.request)
+        //     .then(refresh)
+        // );
 
 });
 
