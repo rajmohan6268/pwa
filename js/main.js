5f0b8704d8e815cb5fe84cce78a57c692c58a70b
rajmohan <rajmohan@techaffinity.com>
2021-05-21 10:24:51 +0530

    init

--- js/main.js
+++ js/main.js
@@ -6,32 +6,58 @@ window.onload = () => {
             .register('./sw.js');
     }
 }
+const prompt = document.querySelector('.prompt');
+const installButton = prompt.querySelector('.prompt__install');
+const closeButton = prompt.querySelector('.prompt__close');
+let installEvent;
 
-let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
+function getVisited() {
+    return localStorage.getItem('install-prompt');
+}
 
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    addBtn.style.display = 'block';
+function setVisited() {
+    localStorage.setItem('install-prompt', true);
+}
 
-    addBtn.addEventListener('click', (e) => {
-        // hide our user interface that shows our A2HS button
-        addBtn.style.display = 'none';
-        // Show the prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-            } else {
-                console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
-        });
+// this event will only fire if the user does not have the pwa installed
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+
+    // if no localStorage is set, first time visitor
+    if (!getVisited()) {
+        // show the prompt banner
+        prompt.style.display = 'block';
+
+        // store the event for later use
+        installEvent = event;
+    }
+});
+
+installButton.addEventListener('click', () => {
+    // hide the prompt banner
+    prompt.style.display = 'none';
+
+    // trigger the prompt to show to the user
+    installEvent.prompt();
+
+    // check what choice the user made
+    installEvent.userChoice.then((choice) => {
+        // if the user declined, we don't want to show the button again
+        // set localStorage to true
+        if (choice.outcome !== 'accepted') {
+            setVisited();
+        }
+
+        installEvent = null;
     });
+});
+
+closeButton.addEventListener('click', () => {
+    // set localStorage to true
+    setVisited();
+
+    // hide the prompt banner
+    prompt.style.display = 'none';
+
+    installEvent = null;
 });
